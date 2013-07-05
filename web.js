703c76659c0b26d146a3f80f895c3087b51a6414
David Janssens <david.jdv@gmail.com>
2013-07-05 22:49:54 +0000

    Update web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World3!');
+  var buffer = fs.readFileSync('./index.html');
+  var hello = buffer.toString("utf8", 0, buffer.length);
+  response.send(hello);
 });
 
 var port = process.env.PORT || 5000;
